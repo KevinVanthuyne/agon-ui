26d91b2c98cb73f756377c74ce2681d850a54075
Kevin Vanthuyne <kevin.vanthuyne@comsof.com>
2022-02-01 21:44:56 +0100

    Added a better marquee (with placeholder text)

--- src/app/components/ticker-item-container/ticker-item-container.component.ts
+++ src/app/components/ticker-item-container/ticker-item-container.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ticker-item-container',
   templateUrl: './ticker-item-container.component.html',
-  styleUrls: ['./ticker-item-container.component.scss']
+  styleUrls: ['./ticker-item-container.component.scss'],
 })
-export class TickerItemContainerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class TickerItemContainerComponent {}
--- src/app/pages/ticker-items/game-of-the-month/game-of-the-month.component.ts
+++ src/app/pages/ticker-items/game-of-the-month/game-of-the-month.component.ts
@@ -1,18 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { TickerItemComponent } from '../ticker-item.component';
 import { GameService } from '../../../services/game.service';
 import { Observable } from 'rxjs';
 import Game from '../../../models/game';
+import { Marquee } from 'dynamic-marquee';
 
 @Component({
   selector: 'app-game-of-the-month',
   templateUrl: './game-of-the-month.component.html',
   styleUrls: ['./game-of-the-month.component.scss'],
 })
-export class GameOfTheMonthComponent implements TickerItemComponent {
+export class GameOfTheMonthComponent
+  implements TickerItemComponent, AfterViewInit
+{
   constructor(private gameService: GameService) {}
+
+  ngAfterViewInit(): void {
+    const marquee = new Marquee(document.getElementById('marquee')!, {
+      rate: -300,
+    });
+    marquee.appendItem('White water');
+    marquee.onAllItemsRemoved(() => {
+      marquee.appendItem('White water');
+    });
+  }
 
   get activeGame$(): Observable<Game> {
     return this.gameService.activeGame$;
+    //   .pipe(
+    //   finalize(() => {
+    //     const marqueeElement = document.getElementById('marquee')!;
+    //     const marquee = new Marquee(marqueeElement);
+    //   })
+    // );
   }
 }
