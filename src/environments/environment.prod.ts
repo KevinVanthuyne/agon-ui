69f0f12336dee271e5e434a94c4d539c2c1352d3
Kevin Vanthuyne <kevin.vanthuyne@comsof.com>
2022-03-29 19:24:08 +0200

    Changed production api url to scores.kevin.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080', // Used in docker-compose
+  apiUrl: 'http://scores.kevinvt.com', // Used in docker-compose. http://localhost:8080 for local docker composing
 };
