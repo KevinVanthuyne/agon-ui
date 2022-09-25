5f4c9676ee1e64bf4cf3b4cf41ff3ce4bcb06345
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2022-09-25 14:44:21 +0200

    Decreased caching interval to 5 seconds
    Showing people's posted score quicker is more fun than waiting 5 minutes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://scores.kevinvt.com',
-  cacheRefreshInterval: 5 * 60 * 1000, // 5 minutes
+  cacheRefreshInterval: 5 * 1000, // 5 seconds
 };
