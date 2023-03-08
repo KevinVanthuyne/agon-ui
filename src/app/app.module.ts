8f1ee1205d5dd7963fa5c5b8f7a767c5e1dec1e1
KevinVanthuyne <kevin.vanthuyne@comsof.com>
2023-03-08 16:10:23 +0100

    Removed game column

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { GameColumnComponent } from './components/game-column/game-column.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HighScoreCardComponent } from './components/high-score-card/high-score-card.component';
 import { LeaderboardPageComponent } from './pages/leaderboard-page/leaderboard-page.component';
@@ -50,7 +49,6 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    GameColumnComponent,
     HighScoreCardComponent,
     LeaderboardPageComponent,
     TickerPageComponent,
