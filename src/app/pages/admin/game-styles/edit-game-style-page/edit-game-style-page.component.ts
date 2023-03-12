cc77ee8ffbfb4b7d6304e33052d51a47a2f98649
KevinVanthuyne <kevin.vanthuyne@comsof.com>
2023-03-12 11:50:39 +0100

    Added a form on style edit page

--- src/app/pages/admin/game-styles/edit-game-style-page/edit-game-style-page.component.ts
+++ src/app/pages/admin/game-styles/edit-game-style-page/edit-game-style-page.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Observable, switchMap } from 'rxjs';
+import { switchMap } from 'rxjs';
 import { GameStyleService } from '../../../../services/game-style.service';
-import GameStyle from '../../../../models/game-style';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-game-style-page',
@@ -10,18 +10,42 @@ import GameStyle from '../../../../models/game-style';
   styleUrls: ['./edit-game-style-page.component.scss'],
 })
 export class EditGameStylePageComponent implements OnInit {
-  protected gameStyle$: Observable<GameStyle> | undefined;
+  form:
+    | FormGroup<{
+        gameId: FormControl<number>;
+        backgroundImage: FormControl<string>;
+        backgroundColor: FormControl<string>;
+        headerImage: FormControl<string>;
+        borderColor: FormControl<string>;
+        fontColor: FormControl<string>;
+      }>
+    | undefined;
 
   constructor(
     private route: ActivatedRoute,
-    private gameStyleService: GameStyleService
+    private gameStyleService: GameStyleService,
+    private fb: FormBuilder
   ) {}
 
   ngOnInit(): void {
-    this.gameStyle$ = this.route.paramMap.pipe(
-      switchMap((params: ParamMap) =>
-        this.gameStyleService.get$(Number.parseInt(params.get('id')!)!)
+    this.route.paramMap
+      .pipe(
+        switchMap((params: ParamMap) =>
+          this.gameStyleService.get$(Number.parseInt(params.get('id')!)!)
+        )
       )
-    );
+      .subscribe((style) => {
+        console.log(style);
+        this.form = this.fb.nonNullable.group({
+          gameId: [style.gameId],
+          backgroundImage: [style.backgroundImage],
+          backgroundColor: [style.backgroundColor],
+          headerImage: [style.headerImage],
+          borderColor: [style.borderColor],
+          fontColor: [style.fontColor],
+        });
+      });
   }
+
+  onSubmit() {}
 }
