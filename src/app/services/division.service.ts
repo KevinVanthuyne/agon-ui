275ae5f004274e8283a97dc7d1f5c10970ab5393
KevinVanthuyne <kevin.vanthuyne@iqgeo.com>
2024-10-30 16:04:13 +0100

    Updated Angular to v18

--- src/app/services/division.service.ts
+++ src/app/services/division.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import {UrlService} from "./url.service";
 import AbstractDivision from "../models/division/abstract-division";
 
