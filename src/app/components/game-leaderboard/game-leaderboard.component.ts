3c74636a02d037d7f4e52cbb371f29c4b208dc2b
KevinVanthuyne <kevin.vanthuyne@iqgeo.com>
2024-10-31 10:57:09 +0100

    Changed game leaderboard to a table

--- src/app/components/game-leaderboard/game-leaderboard.component.ts
+++ src/app/components/game-leaderboard/game-leaderboard.component.ts
@@ -1,22 +1,26 @@
-import {Component, Input, OnInit} from '@angular/core';
-import Game from "../../models/game";
-import {ScoreService} from "../../services/score.service";
-import {Observable, of} from "rxjs";
-import Score from "../../models/score";
-import {DivisionService} from "../../services/division.service";
-import AbstractDivision from "../../models/division/abstract-division";
+import { Component, Input, OnInit } from '@angular/core';
+import Game from '../../models/game';
+import { ScoreService } from '../../services/score.service';
+import { Observable, of } from 'rxjs';
+import Score from '../../models/score';
+import { DivisionService } from '../../services/division.service';
+import AbstractDivision from '../../models/division/abstract-division';
 
 @Component({
   selector: 'app-game-leaderboard',
   templateUrl: './game-leaderboard.component.html',
-  styleUrls: ['./game-leaderboard.component.scss']
+  styleUrls: ['./game-leaderboard.component.scss'],
 })
-export class GameLeaderboardComponent implements OnInit{
+export class GameLeaderboardComponent implements OnInit {
   @Input() game!: Game;
-  divisions$: Observable<AbstractDivision[]> = of([]);
 
-  constructor(private readonly scoreService: ScoreService, private readonly divisionService: DivisionService) {
-  }
+  protected divisions$: Observable<AbstractDivision[]> = of([]);
+  protected displayedColumns = ['rank', 'name', 'score', 'timestamp'];
+
+  constructor(
+    private readonly scoreService: ScoreService,
+    private readonly divisionService: DivisionService
+  ) {}
 
   ngOnInit(): void {
     this.divisions$ = this.divisionService.getDivisionsForGame$(this.game.id);
